49e7d306bf7299185e76bcfc3d7ebc5da27f518f
mbauer599 <mbauer599@users.noreply.github.com>
2017-11-08 10:21:00 -0600

    added help, dns, addr

--- configure/configure-network.sh
+++ configure/configure-network.sh
@@ -1,1 +1,62 @@
 #!/bin/bash
+
+while test $# -gt 0; do
+        case "$1" in
+                -h|--help)
+                        echo "Configure Network Settings"
+                        echo " "
+                        echo "options:"
+                        echo "-h, --help                This help menu"
+                        echo "-a, --addr=               Set IP address"
+                        echo "-d, --dns=                Set System DNS"
+                        echo "-g, --gw=                 Set GateWay"
+                        echo "-n, --name=               Set System HostName"
+                        exit 0
+                        ;;
+                -a)
+                        shift
+                        if test $# -gt 0; then
+                                export PROCESS=$1
+                        else
+                                echo "no process specified"
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                --addr*)
+                        export PROCESS=`echo $1 | sed -e 's/^[^=]*=//g'`
+                        shift
+                        ;;
+                -d)
+                        shift
+                        if test $# -gt 0; then
+                                export PROCESS=$1
+                        else
+                                echo "no process specified"
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                --dns*)
+                        export PROCESS=`echo $1 | sed -e 's/^[^=]*=//g'`
+                        shift
+                        ;;
+                -o)
+                        shift
+                        if test $# -gt 0; then
+                                export OUTPUT=$1
+                        else
+                                echo "no output dir specified"
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                --output-dir*)
+                        export OUTPUT=`echo $1 | sed -e 's/^[^=]*=//g'`
+                        shift
+                        ;;
+                *)
+                        break
+                        ;;
+        esac
+done
